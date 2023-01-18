998521eaade31c0d9c9ed3e47212bac0ab585f13
Antonio Aldisio <aldisiofilho@gmail.com>
2023-01-17 22:36:10 -0300

    comunicacao

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "../inc/Uart.h"
+
+
+const int FORNO = 4;
+const int FAN = 5;
+
+bool working = true;
+
+int main(void){
+    Uart uart;
+    uart.setup();
+
+    while(working){
+        int retorno = uart.getUserInput();
+        printf("%d", retorno);
+    }
+}
