558525500159e752e7dc756572ac7ff94c5ffcaa
antonioaldisio <aldsiofilho@gmail.com>
2023-01-21 02:39:17 -0300

    logs

--- src/main.cpp
+++ src/main.cpp
@@ -36,16 +36,18 @@ void setupPin(){
 
 
 void status(double intensidade){
+    printf("intensidade: %f \n", intensidade);
     if (intensidade >= 0){
         softPwmWrite(FORNO, intensidade);
         softPwmWrite(VENTOINHA, 0);
     }else{
         if(intensidade > -40){
+            intensidade = 40;
             softPwmWrite(FORNO, 40);
             softPwmWrite(VENTOINHA, 0);
         }else{
-            double aux = intensidade * (-1);
-            softPwmWrite(VENTOINHA, aux);
+            intensidade = intensidade * (-1);
+            softPwmWrite(VENTOINHA, intensidade);
             softPwmWrite(FORNO, 0);
         }
     }
@@ -60,12 +62,16 @@ void esquenta(Uart uart, Pid pid, double *intensidade){
     status(*intensidade);
 
     while(working && temInter <= tempRef){
-        printf("Estou Esquetando \n\n");
+        
+        printf("Estou Esquetando \n");
         printf("temInter %f\n", temInter);
-        printf("ambTemp %f\n", tempRef);
+        printf("ambTemp %f\n\n\n", tempRef);
+
         tempRef = uart.getReferenceTemp();
         temInter = uart.getInternalTemp();
+        
         *intensidade = pid.pid_controle(temInter);
+        status(*intensidade);
         uart.sendControlSignal((int)*intensidade);
         sleep(10);
     }
@@ -74,16 +80,20 @@ void esquenta(Uart uart, Pid pid, double *intensidade){
 void esfriando(Uart uart, Sensor Sensor, Pid pid, double *intensidade){
     double ambTemp = Sensor.getSensorTemp(&bme280);
     float temInter = uart.getInternalTemp();
+    float tempRef = uart.getReferenceTemp();
+
     *intensidade = 100.0;
     status(*intensidade);
 
     while(working && temInter >= ambTemp){
-        printf("Estou esfriando \n\n");
+        
+        printf("Estou esfriando\n");
         printf("temInter %f\n", temInter);
-        printf("ambTemp %f\n", ambTemp);
+        printf("ambTemp %f\n\n\n", ambTemp);
+        
         ambTemp = Sensor.getSensorTemp(&bme280);
         temInter = uart.getInternalTemp();
-        *intensidade = pid.pid_controle(temInter);
+        softPwmWrite(VENTOINHA, 100);
         uart.sendControlSignal((int)*intensidade);
         sleep(10);
     }
@@ -95,6 +105,8 @@ int main(void){
     Pid pid;
 
     double intensidade = 0;
+    bool ligado = false;
+    bool execucao = false;
 
     // SetUp
     uart.setup();
@@ -109,29 +121,41 @@ int main(void){
         int retorno = uart.getUserInput();
         printf("%d \n", retorno);
         if (retorno == 161){
-            printf("Ligado \n");
-            uart.setSystemState(1);
-
+            if (!ligado){
+                ligado = true;
+                printf("Ligado \n");
+                uart.setSystemState(1);
+            }
         }
         else if (retorno == 162){
-            printf("Desligado \n");
-            uart.setSystemState(0);
+            if (ligado){
+                ligado = false;
+                printf("Desligado \n");
+                uart.setSystemStatus(0);
+                uart.setSystemState(0);
+            }
         }
         else if (retorno == 163){
-            printf("Iniciado \n");
-            uart.setSystemStatus(1);
-            esquenta(uart, pid, &intensidade);
-            esfriando(uart, sensor, pid, &intensidade);
-
+            if (!execucao){
+                execucao = true;
+                printf("Iniciado \n");
+                uart.setSystemStatus(1);
+                esquenta(uart, pid, &intensidade);
+                esfriando(uart, sensor, pid, &intensidade);
+            }
         }
         else if (retorno == 164){
-            printf("Parado \n");
-            uart.setSystemStatus(0);
-
+            if (execucao){
+                execucao = false;
+                printf("Parado \n");
+                uart.setSystemStatus(0);
+            }
         }
+        // TODO
         else if (retorno == 165){
             printf("Manual \n");
         }
+
         else {
             usleep(1000000);
             // printf("%d \n", retorno);
